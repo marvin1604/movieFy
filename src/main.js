bb109af658fc99a117e78adcaa1e9b6c127530ff
marvin1604 <walter.rodriguez1992@gmail.com>
2022-11-16 17:26:03 -0500

    agregando carrusel de peliculas

--- src/main.js
+++ src/main.js
@@ -1,3 +1,21 @@
 console.log("a codear");
 
 console.log('API URL = https: jhdjsk.com/hdhjs?api_key=' + API_KEY)
+
+const grande = document.querySelector(".grande");
+const punto = document.querySelectorAll(".punto");
+
+//cuando click en punto
+
+punto.forEach((cadapunto, i) =>{
+    punto[i].addEventListener("click", () =>{
+        let posicion = i;
+        let operacion = (posicion * -25)+35;
+
+        grande.style.transform = `translateX(${operacion}%)`
+        punto.forEach((cadapunto, i) =>{
+            punto[i].classList.remove("activo");
+        })
+        punto[i].classList.add("activo");
+    })
+})
