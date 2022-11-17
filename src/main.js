478be71685badcbf3d106e26a74dea7fa2494c2d
marvin1604 <walter.rodriguez1992@gmail.com>
2022-11-16 23:39:33 -0500

    trayendo informacion de la APi para peliculas y categorias

--- src/main.js
+++ src/main.js
@@ -1,12 +1,57 @@
 console.log("a codear");
 
-console.log('API URL = https: jhdjsk.com/hdhjs?api_key=' + API_KEY)
+const baseURL= "https://api.themoviedb.org/3"
 
 const grande = document.querySelector(".grande");
 const punto = document.querySelectorAll(".punto");
+const containerImagenes = document.querySelector(".container-imagenes");
+const anterior = document.querySelector(".anterior")
+const siguiente = document.querySelector(".siguiente");
+const listaCategories= document.querySelector(".lista");
 
-//cuando click en punto
+async function getTendenciaMoviesPreview(){
+    const res = await fetch(`${baseURL}/trending/movie/day?api_key=` + API_KEY);
+    const data = await res.json();
+    const movies = data.results;
+    movies.forEach(movie =>{
+        const imagenUrl= "https://image.tmdb.org/t/p/w300/";
+        // console.log(movie.poster_path);
+        const imagen = document.createElement("img");
+        imagen.src=  imagenUrl+movie.poster_path;
+        containerImagenes.appendChild(imagen);
+    })
+}
 
+async function getTendencyCarruselPreview(){
+    const res = await fetch(`${baseURL}/trending/movie/week?api_key=` + API_KEY)
+    const data = await res.json();
+    const movies = data.results;
+    for(let item =0;  item< 3; item++){
+        // console.log(movies[item].poster_path)
+        const imagen = document.createElement("img");
+        imagen.className= "img"
+        imagen.src=  "https://image.tmdb.org/t/p/w300/"+ movies[item].poster_path;
+        grande.appendChild(imagen);
+    }
+}
+async function getCategory(){
+    const res = await fetch(`${baseURL}/genre/movie/list?api_key=` + API_KEY)
+    const data = await res.json();
+    const categories = data.genres;
+    categories.forEach(category =>{
+        console.log(category.name);
+        const button = document.createElement("button");
+        button.innerText = category.name;
+        listaCategories.appendChild(button);
+
+    })
+}
+
+getTendenciaMoviesPreview();
+getTendencyCarruselPreview();
+// getCategory();
+
+//carrusel inicio
 punto.forEach((cadapunto, i) =>{
     punto[i].addEventListener("click", () =>{
         let posicion = i;
@@ -19,3 +64,25 @@ punto.forEach((cadapunto, i) =>{
         punto[i].classList.add("activo");
     })
 })
+
+//carrusel tendencias
+siguiente.addEventListener("click", showDerecha);
+var inicio = 0;
+function showDerecha(){
+    inicio = ((inicio -50));
+    if (inicio > -400){
+        containerImagenes.style.transform = `translateX(${inicio}%)`
+    }else{
+        inicio=-400;
+    }
+}
+
+anterior.addEventListener("click", showIzquierda);
+function showIzquierda(){
+    inicio = ((inicio +50));
+    if(inicio <= 0){
+        containerImagenes.style.transform = `translateX(${inicio}%)`
+    }else{
+        inicio=0;
+    }
+}
